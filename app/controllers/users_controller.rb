1a92384cbef33535040bc25635a666583acaf939
Eero Hyyti <eero.hyyti@gmail.com>
2013-11-19 13:49:39 +0200

    Until 7.4.4, Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -23,4 +24,3 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 end
-
